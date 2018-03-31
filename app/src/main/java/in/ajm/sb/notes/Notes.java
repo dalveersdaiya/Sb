00fb2a4cb8e39a037cac2db0ab6652c2f75f599a
dalveersdaiya <dalveersinghdaiya@gmail.com>
2018-03-31 17:16:39 +0530

    Sms verify lib integration

---
+++ app/src/main/java/in/ajm/sb/notes/Notes.java
@@ -0,0 +1,74 @@
+package in.ajm.sb.notes;
+
+public class Notes {
+    /**
+     * //    Hack : How to use SmS Verify catcher
+     *
+     * public class MainActivity extends AppCompatActivity {
+     private SmsVerifyCatcher smsVerifyCatcher;
+
+     @Override protected void onCreate(Bundle savedInstanceState) {
+     super.onCreate(savedInstanceState);
+     setContentView(R.layout.activity_main);
+
+     //init views
+     final EditText etCode = (EditText) findViewById(R.id.et_code);
+     final Button btnVerify = (Button) findViewById(R.id.btn_verify);
+
+     //init SmsVerifyCatcher
+     smsVerifyCatcher = new SmsVerifyCatcher(this, new OnSmsCatchListener<String>() {
+     @Override public void onSmsCatch(String message) {
+     String code = parseCode(message);//Parse verification code
+     etCode.setText(code);//set code in edit text
+     //then you can send verification code to server
+     }
+     });
+
+     //set phone number filter if needed
+     smsVerifyCatcher.setPhoneNumberFilter("777");
+     //smsVerifyCatcher.setFilter("regexp");
+
+     //button for sending verification code manual
+     btnVerify.setOnClickListener(new View.OnClickListener() {
+     @Override public void onClick(View v) {
+     //send verification code to server
+     }
+     });
+     }
+
+      * Parse verification code
+      *
+      * @param message sms message
+     * @return only four numbers from massage string
+    private String parseCode(String message) {
+    Pattern p = Pattern.compile("\\b\\d{4}\\b");
+    Matcher m = p.matcher(message);
+    String code = "";
+    while (m.find()) {
+    code = m.group(0);
+    }
+    return code;
+    }
+
+     @Override protected void onStart() {
+     super.onStart();
+     smsVerifyCatcher.onStart();
+     }
+
+     @Override protected void onStop() {
+     super.onStop();
+     smsVerifyCatcher.onStop();
+     }
+
+      * need for Android 6 real time permissions
+     @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+     smsVerifyCatcher.onRequestPermissionsResult(requestCode, permissions, grantResults);
+     }
+     }
+
+     *
+     */
+
+
+}
