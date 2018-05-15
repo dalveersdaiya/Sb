7744d77efd9d8f77d5dd1e9414bc4575a492f4e9
dalveersdaiya <dalveersinghdaiya@gmail.com>
2018-05-15 18:22:18 +0530

    added a test widget

---
+++ app/src/main/java/in/ajm/sb/schoolwidget/TestWidgetDataProvider.java
@@ -0,0 +1,125 @@
+package in.ajm.sb.schoolwidget;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.util.TypedValue;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.ajm.sb.R;
+
+public class TestWidgetDataProvider implements RemoteViewsService.RemoteViewsFactory {
+
+    private static final String TAG = "WidgetDataProvider";
+
+    List<String> mCollection = new ArrayList<>();
+    Context mContext = null;
+
+    public TestWidgetDataProvider(Context context, Intent intent) {
+        mContext = context;
+    }
+
+    public static Bitmap getFontBitmap(Context context, String text, int color, float fontSizeSP) {
+        int fontSizePX = convertDiptoPix(context, fontSizeSP);
+        int pad = (fontSizePX / 9);
+        Paint paint = new Paint();
+        Typeface typeface = Typeface.createFromAsset(context.getAssets(), "Fonts/Mark Simonson - Proxima Nova Alt Regular-webfont.ttf");
+        paint.setAntiAlias(true);
+        paint.setTypeface(typeface);
+        paint.setColor(color);
+        paint.setTextSize(fontSizePX);
+
+        int textWidth = (int) (paint.measureText(text) + pad * 2);
+        int height = (int) (fontSizePX / 0.75);
+        Bitmap bitmap = Bitmap.createBitmap(textWidth, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        float xOriginal = pad;
+        canvas.drawText(text, xOriginal, fontSizePX, paint);
+        return bitmap;
+    }
+
+    public static int convertDiptoPix(Context context, float dip) {
+        int value = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, context.getResources().getDisplayMetrics());
+        return value;
+    }
+
+    @Override
+    public void onCreate() {
+        initData();
+    }
+
+    @Override
+    public void onDataSetChanged() {
+        initData();
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public int getCount() {
+        return mCollection.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews view = new RemoteViews(mContext.getPackageName(),
+                R.layout.test_widget_list_item);
+        view.setTextViewText(R.id.name_txt, mCollection.get(position));
+        return view;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    private void initData() {
+        mCollection.clear();
+        for (int i = 1; i <= 10; i++) {
+            mCollection.add("School book notice " + i);
+        }
+    }
+
+    public Bitmap buildUpdate(String time) {
+        Bitmap myBitmap = Bitmap.createBitmap(160, 84, Bitmap.Config.ARGB_8888);
+        Canvas myCanvas = new Canvas(myBitmap);
+        Paint paint = new Paint();
+        Typeface clock = Typeface.createFromAsset(mContext.getAssets(), "Mark Simonson - Proxima Nova Alt Regular-webfont.ttf");
+        paint.setAntiAlias(true);
+        paint.setSubpixelText(true);
+        paint.setTypeface(clock);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        paint.setTextSize(65);
+        paint.setTextAlign(Paint.Align.CENTER);
+        myCanvas.drawText(time, 80, 60, paint);
+        return myBitmap;
+    }
+
+}
